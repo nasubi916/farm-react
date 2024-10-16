23bf752f042ce79af8feccc9132662dce6ecef6b
nasubi916 <r916nis@gmail.com>
2024-10-16 10:56:24 +0900

    モジュールの型宣言の謎

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,18 @@
 import { useStore } from "@nanostores/react";
 import { type ReactElement } from "react";
 import { $users } from "@/stores/useUser";
+import { type User } from "@/types/user";
 
 export function About(): ReactElement {
-  const users = useStore($users);
-  return <>About : {users.map((user) => user)}</>;
+  const users: User[] = useStore($users);
+  return (
+    <div>
+      <h1>About</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.name}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
